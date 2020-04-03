27f5ddd4b5eae92871aec037ca228d1542c3187c
Celal <cllakyz@hotmail.com>
2020-04-03 23:36:44 +0300

    arrow functions

---
+++ es6/arrow-functions.js
@@ -0,0 +1,12 @@
+/*var func = function () {
+    return 1;
+};*/
+
+/*const func = (x, y) => {
+    return x + y;
+};*/
+
+//const func = (x, y) => x + y;
+const func = x => x + 5;
+
+console.log(func(3));
