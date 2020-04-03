f34433b51c4e9c846e039f938b2a563834c23e03
Celal <cllakyz@hotmail.com>
2020-04-03 17:10:43 +0300

    aritmetik operatörler

---
+++ temel-js/aritmetik-operatorler.js
@@ -0,0 +1,12 @@
+var number = 5;
+var newNumber = (number *= 2);
+
+console.log(number + 3);
+console.log(number + "3");
+console.log(number - 3);
+console.log(number * 3);
+console.log(number / 3);
+console.log(number % 3);
+console.log(number++);
+console.log(++number);
+console.log(newNumber);
