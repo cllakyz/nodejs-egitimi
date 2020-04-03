7fc251aa0269ffa333bdf4412327276d4742c219
Celal <cllakyz@hotmail.com>
2020-04-03 22:55:10 +0300

    es6 giriş

---
+++ es6/let-const.js
@@ -0,0 +1,34 @@
+/*
+var x = "degisken";
+let y = "bu da degisken";
+
+//for (var i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
+    //
+}
+
+console.log(i);
+*/
+
+//var x = 'foo';
+//var x = 'bar';
+
+/*
+let x = 'foo';
+x = 'bar';
+
+console.log(x);
+*/
+
+/*
+//const pi = 3;
+let pi = 3;
+pi = 3.14;
+
+console.log(pi);
+*/
+
+const arr = [1, 2, 3, 4];
+arr.push(5);
+
+console.log(arr);
