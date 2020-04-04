f950e202ffb7381469d09b4d089a46f94e8f6788
Celal <cllakyz@hotmail.com>
2020-04-04 23:33:39 +0300

    server işlemleri

---
+++ http/create-http-server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    console.log("Bir istekte bulunuldu.");
+    console.log(request.headers);
+    response.writeHead(200, {
+        'content-type': 'text/html; charset=utf-8'
+    });
+    response.write(`<b>Merhaba</b> Dünya`);
+    response.end();
+});
+
+server.listen(3000);
