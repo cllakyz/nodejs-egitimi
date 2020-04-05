6512ed5daf47373f64bbf68f6b3f8ef144aba6e5
Celal <cllakyz@hotmail.com>
2020-04-05 23:52:24 +0300

    express hata yönetimi

--- express/middleware/app.js
+++ express/middleware/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 /* Routers */
 const user = require('./routes/user');
 const profile = require('./routes/profile');
@@ -12,6 +14,14 @@ app.use(isLogin);*/
 
 app.use('/user', user);
 app.use('/profile', profile);
+
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.render('error', {
+        message: err.message,
+        status: err.status,
+    });
+});
 
 app.listen(3000, () => {
     console.log('express server çalıştı.');
--- express/middleware/routes/user.js
+++ express/middleware/routes/user.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('User Sayfası');
+router.get('/', (req, res, next) => {
+    const user = false;
+    if (user)
+        res.send('User Sayfası');
+    else
+        return next({
+            status: 404,
+            message: "Kullanıcı Bulunamadı"
+        });
 });
 
 module.exports = router;
