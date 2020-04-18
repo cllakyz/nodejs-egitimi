0c30d88700bd2cbabb44eaad560fa3368aa973c2
Celal <cllakyz@hotmail.com>
2020-04-18 17:27:54 +0300

    all rooms list

--- websocket/room/app.js
+++ websocket/room/app.js
@@ -11,11 +11,20 @@ const io = socketio.listen(server);
 io.on('connection', (socket) => {
     console.log(socket.id);
 
+    socket.join('room1');
+    socket.join('room2');
+    socket.join('room3', () => {
+        const rooms = getOnlinedRomms(socket);
+        console.log(rooms);
+    });
+
     socket.on('joinRoom', (data) => {
         socket.join(data.name, () => {
             // socket.to(data.name).emit('newJoin', { count: getOnlineCount(data) }); // odaya giren kişi hariç diğer kişilere emit eder.
             io.to(data.name).emit('newJoin', { count: getOnlineCount(data) }); // odaya giren kişi dahil herkese emit eder.
             socket.emit('log', { message: 'Odaya girdiniz.' });
+            const rooms = getOnlinedRomms(socket);
+            console.log(rooms);
         });
     });
 
@@ -23,6 +32,8 @@ io.on('connection', (socket) => {
         socket.leave(data.name, () => {
             io.to(data.name).emit('leavedRoom', { count: getOnlineCount(data) });
             socket.emit('logLeaved', { message: 'Odadan ayrıldınız.' });
+            const rooms = getOnlinedRomms(socket);
+            console.log(rooms);
         });
     });
 });
@@ -31,3 +42,5 @@ const getOnlineCount = (data) => {
     const room = io.sockets.adapter.rooms[data.name];
     return room ? room.length : 0;
 };
+
+const getOnlinedRomms = (socket) => Object.keys(socket.rooms);
